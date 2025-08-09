039aee63abf4455a8b29d993f4b05f46f2871201
Burncake <ltmtri22@clc.fitus.edu.vn>
2025-08-09 13:16:16 +0700

    Update site2

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Java
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre -y
+# Jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+# Git
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git -y
+# Docker
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo usermod -aG docker jenkins
+sudo systemctl restart jenkins
